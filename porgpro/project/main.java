2377a7bdc10a8b39c5238abf04533846c3e7e141
kkck94 <kenan.kciku@mni.thm.de>
2025-07-04 13:55:02 +0200

    added Markdown firts try

--- porgpro/project/main.java
+++ porgpro/project/main.java
@@ -1,28 +1,37 @@
 void main(){
 
-		Tokenizer TO = new Tokenizer("x 2 ^ x +");
-		
-		Token [] t = TO.maker();
-
-		FunctionResolver fr = new FunctionResolver();
-		
-		Token [] Z = fr.resolveAll(t);
-
-		Plotter PO = new Plotter();
-
-		PO.drawFunc(Z);
-
-		UPNParser P = new UPNParser(Z);
-
-		Expr g = P.parse();
-
-		CalcTree U = new CalcTree(g);
-
-		TreeMaker TM = new TreeMaker();
-
-		String h = TM.planter(g);
-
  Clerk.clear();
+
+    String exampleValue; // Input Example
+    Clerk.write(Interaction.input("./Combined.java", "// Input String Example", "String exampleString = \"$\";", "Geben Sie einen UPN ausdruck ein"));
+
+    Clerk.markdown(Text.fillOut(""" 
+		## Hier sehen sie den SyntaxBaum zum arithmetischen Ausdruck
+		""", Text.codeBlock("./Plotter.java" , "//Input")));
+
+	Tokenizer TO = new Tokenizer(exampleValue);
+	
+	Token [] t = TO.maker();
+
+	FunctionResolver fr = new FunctionResolver();
+	
+	Token [] Z = fr.resolveAll(t);
+
+	Plotter PO = new Plotter();
+
+	PO.drawFunc(Z);
+	
+	UPNParser P = new UPNParser(Z);
+
+	Expr g = P.parse();
+
+	CalcTree U = new CalcTree(g);
+
+	TreeMaker TM = new TreeMaker();
+
+	String h = TM.planter(g);
+
+
 
     // Dot
     Dot dot = new Dot();
